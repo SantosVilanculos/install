ad8f9956157bb5f25ed35382d90eeca6c385db07
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-01-10 16:56:50 +0200

    chore: do not change mariadb password

--- ubuntu_24.sh
+++ ubuntu_24.sh
@@ -142,10 +142,6 @@ sudo systemctl restart apache2.service
 # ---
 sudo apt-get install -y mariadb-server
 
-sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';"
-sudo mysql -e "CREATE DATABASE IF NOT EXISTS laravel;"
-sudo mysql -e "FLUSH PRIVILEGES;"
-
 sudo sed -i 's/^bind-address\s*=\s*127\.0\.0\.1/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 sudo systemctl restart mariadb
