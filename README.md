04ac8ab5e81ef33a8a1c5de71aa0ad20ab28a239
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-01-25 03:06:43 +0200

    chore: update debian_12.sh

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+[![last commit](https://img.shields.io/github/last-commit/SantosVilanculos/install)](https://github.com/SantosVilanculos/install/commits/main)
+[![license](https://img.shields.io/github/license/SantosVilanculos/install)](https://github.com/SantosVilanculos/install/blob/main/LICENSE)
+
 ```sh
 curl -sSfL https://raw.githubusercontent.com/SantosVilanculos/install/main/debian_12.sh | sh
 ```
--- debian_12.sh
+++ debian_12.sh
@@ -56,77 +56,74 @@ sudo apt-get upgrade -y
 # sudo apt-get dist-upgrade -y
 
 # ---
-sudo apt-get install -y zsh
-sudo chsh -s "/usr/bin/zsh"
-
-# ---
 sudo apt-get install -y libfuse2
 sudo apt-get install -y flatpak
 sudo apt-get install -y gnome-software-plugin-flatpak
 sudo flatpak remote-add --if-not-exists flathub "https://dl.flathub.org/repo/flathub.flatpakrepo"
 
 # ---
+sudo apt-get install -y zsh
+sudo chsh -s "/usr/bin/zsh"
+
+# ---
 sudo apt-get install -y tmux
+sudo apt-get install -y vim
 
 # ---
-sudo apt-get install -y i3
-sudo apt-get install -y nitrogen
-sudo apt-get install -y dmenu
-sudo apt-get install -y rofi
-sudo apt-get install -y policykit-1-gnome
-
-# ---
-sudo apt-get install -y ntfs-3g
-
-# ---
-sudo apt-get install -y software-properties-common
 sudo apt-get install -y build-essential
-sudo apt-get install -y git
-sudo apt-get install -y gh
-
-sudo apt-get install -y curl
-sudo apt-get install -y wget
-sudo apt-get install -y zip
-sudo apt-get install -y unzip
-
-sudo apt-get install -y ffmpeg
-sudo apt-get install -y vlc
-sudo apt-get install -y maim
-
-sudo apt-get install -y fzf
-sudo apt-get install -y xsel
-sudo apt-get install -y xclip
-sudo apt-get install -y libnss3-tools
-
-sudo apt-get install -y gparted
-sudo apt-get install -y net-tools
-sudo apt-get install -y java-common
-
-sudo apt-get install -y gpg
-sudo apt-get install -y jq
-sudo apt-get install -y openssl
 sudo apt-get install -y ca-certificates
-
-sudo apt-get install -y gnome-tweaks
-sudo apt-get install -y gnome-shell-extensions
 sudo apt-get install -y chrome-gnome-shell
+sudo apt-get install -y curl
+sudo apt-get install -y fd-find
+sudo apt-get install -y ffmpeg
+sudo apt-get install -y fzf
+sudo apt-get install -y gh
+sudo apt-get install -y git
+sudo apt-get install -y gnome-shell-extensions
+sudo apt-get install -y gnome-tweaks
+sudo apt-get install -y gparted
+sudo apt-get install -y gpg
+sudo apt-get install -y java-common
+sudo apt-get install -y jq
+sudo apt-get install -y libnss3-tools
+sudo apt-get install -y maim
+sudo apt-get install -y net-tools
+sudo apt-get install -y ntfs-3g
+sudo apt-get install -y openssh-client
+sudo apt-get install -y openssl
+sudo apt-get install -y qt5-wayland
+sudo apt-get install -y qt6-wayland
+sudo apt-get install -y ripgrep
+sudo apt-get install -y software-properties-common
+sudo apt-get install -y unzip
+sudo apt-get install -y vlc
+sudo apt-get install -y wget
+sudo apt-get install -y xclip
+sudo apt-get install -y xsel
+sudo apt-get install -y zip
 sudo flatpak install -y flathub com.mattjakeman.ExtensionManager
-
 sudo flatpak install -y flathub it.mijorus.gearlever
 
 # ---
-cd "/tmp"
-
-wget "https://github.com/fastfetch-cli/fastfetch/releases/download/2.20.1/fastfetch-linux-amd64.deb" -O ./fastfetch-linux-amd64.deb
-sudo dpkg -i ./fastfetch-linux-amd64.deb
-
-cd -
+sudo apt-get install -y i3
+sudo apt-get install -y dmenu
+sudo apt-get install -y nitrogen
+sudo apt-get install -y policykit-1-gnome
+sudo apt-get install -y rofi
 
 # ---
 cd "/tmp"
 
 wget "https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.6.0/gcm-linux_amd64.2.6.0.deb" -O ./gcm-linux_amd64.2.6.0.deb
 sudo dpkg -i ./gcm-linux_amd64.2.6.0.deb
+
+cd -
+
+# ---
+cd "/tmp"
+
+wget "https://github.com/fastfetch-cli/fastfetch/releases/download/2.20.1/fastfetch-linux-amd64.deb" -O ./fastfetch-linux-amd64.deb
+sudo dpkg -i ./fastfetch-linux-amd64.deb
 
 cd -
 
@@ -140,6 +137,30 @@ echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable
 sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
 sudo apt-get update
 sudo apt-get install -y eza
+
+# ---
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt-get update
+
+sudo apt-get install -y --ignore-missing \
+  php8.2 php8.2-{cli,common,fpm,mysql,sqlite3,pgsql,zip,gd,mbstring,curl,xml,bcmath,tokenizer,intl,tidy,imagick,mcrypt}
+
+# ---
+sudo apt-get install -y apache2 libapache2-mod-php
+sudo systemctl enable apache2.service
+sudo systemctl start apache2.service
+sudo a2enmod rewrite
+
+echo "DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm" | sudo tee /etc/apache2/mods-enabled/dir.conf
+
+sudo systemctl restart apache2.service
+
+# ---
+sudo apt-get install -y mariadb-server
+
+sudo sed -i 's/^bind-address\s*=\s*127\.0\.0\.1/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+sudo systemctl restart mariadb
 
 # ---
 cd "/tmp"
@@ -170,32 +191,11 @@ sudo systemctl enable mailpit.service
 sudo systemctl start mailpit.service
 
 # ---
-sudo add-apt-repository -y ppa:ondrej/php
-sudo apt-get update
+sudo flatpak install -y flathub app.zen_browser.zen
 
-sudo apt-get install -y --ignore-missing \
-    php8.2 php8.2-{cli,common,fpm,mysql,sqlite3,pgsql,zip,gd,mbstring,curl,xml,bcmath,tokenizer,intl,tidy,imagick,mcrypt}
+sudo ln -s /var/lib/flatpak/exports/bin/app.zen_browser.zen /usr/local/bin/zen
 
-# ---
-sudo apt-get install -y apache2 libapache2-mod-php
-sudo systemctl enable apache2.service
-sudo systemctl start apache2.service
-sudo a2enmod rewrite
-
-echo "DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm" | sudo tee /etc/apache2/mods-enabled/dir.conf
-
-sudo systemctl restart apache2.service
-
-# ---
-sudo apt-get install -y mariadb-server
-
-sudo sed -i 's/^bind-address\s*=\s*127\.0\.0\.1/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
-
-sudo systemctl restart mariadb
-
-# ---
-sudo flatpak install -y flathub io.github.zen_browser.zen
-xdg-mime default io.github.zen_browser.zen.desktop x-scheme-handler/http
-xdg-mime default io.github.zen_browser.zen.desktop x-scheme-handler/https
-xdg-mime default io.github.zen_browser.zen.desktop text/html
-xdg-settings set default-web-browser io.github.zen_browser.zen.desktop
+xdg-mime default app.zen_browser.zen.desktop x-scheme-handler/http
+xdg-mime default app.zen_browser.zen.desktop x-scheme-handler/https
+xdg-mime default app.zen_browser.zen.desktop text/html
+xdg-settings set default-web-browser app.zen_browser.zen.desktop
