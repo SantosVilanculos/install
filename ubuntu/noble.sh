c84ee816c648238bb8b6f73a7b830287a0d8813c
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-03-08 05:49:12 +0200

    chore: change mailpit service description

--- ubuntu/noble.sh
+++ ubuntu/noble.sh
@@ -116,7 +116,7 @@ cd -
 
 cat <<'EOF' | sudo tee /etc/systemd/system/mailpit.service
 [Unit]
-Description=Mailpit is an email testing tool for developers
+Description=An email testing tool capturing emails from your application during development.
 
 [Service]
 ExecStart=/usr/local/bin/mailpit
