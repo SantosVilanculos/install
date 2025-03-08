cb4dc9e8ca5df163b3c2ae2cc03eb759d596b82a
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-03-08 04:23:17 +0200

    chore: install redis

--- debian/bookworm.sh
+++ debian/bookworm.sh
@@ -169,6 +169,16 @@ sudo apt update
 sudo apt -y install postgresql
 
 # ---
+sudo apt-get install lsb-release curl gpg
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+sudo chmod 644 /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+sudo apt-get update
+sudo apt-get install -y redis
+sudo systemctl enable redis-server
+sudo systemctl start redis-server
+
+# ---
 cd "/tmp"
 wget https://github.com/axllent/mailpit/releases/download/v1.21.1/mailpit-linux-amd64.tar.gz -O "./mailpit-linux-amd64.tar.gz"
 mkdir -p ./mailpit-linux-amd64
